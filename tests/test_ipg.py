4204e23ffbae835a3d3f343f222bc413aac4db22
Javad <javadalipanah@gmail.com>
2024-04-01 23:20:29 +0330

    add ipg transaction info retrieval/check and verification tests

--- tests/test_ipg.py
+++ tests/test_ipg.py
@@ -11,3 +11,18 @@ def test_get_redirect_url(client):
     token = "test_token"
     redirect_url = client.ipg.get_redirect_url(token)
     assert redirect_url == IPGEndpoint.redirect.format(token=token)
+
+
+def test_get_transaction_info(client):
+    token = "test_token"
+    transaction = client.ipg.get_transaction_info(token)
+    assert transaction.is_successful()
+    assert transaction.is_card_number_valid("1234123412341238")
+    assert transaction.to_dict() == client.get_request_strategy().endpoints[IPGEndpoint.info]["POST"]["response"]
+
+
+def test_verify_transaction(client):
+    token = "test_token"
+    transaction = client.ipg.verify_transaction(token)
+    assert transaction.is_successful()
+    assert transaction.to_dict() == client.get_request_strategy().endpoints[IPGEndpoint.verify]["POST"]["response"]
